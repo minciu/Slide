79a33741460a7d2edb3723626f2a954f161f6615
minciu <stefan_minciuna@yahoo.com>
2020-04-30 20:44:33 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,69 @@
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './App.css';
+import Slider from "react-slick";
+
+function App() {
+  const [suggestions,setSuggestions]=useState([])
+
+
+  useEffect(()=>{
+fetch('https://jsonplaceholder.typicode.com/users').then(res=>res.json()).then(data=>{
+  setSuggestions(data);
+})
+  });
+
+  let settings = {
+    infinite: false,
+    speed: 1000,
+    arrows:true,
+    
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    
+    responsive: [
+      {
+        breakpoint: 960,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 2
+        }
+    }
+  ]
+  }
+      return (
+        <div className="container">
+            <h6 className="text-muted">Imagini</h6>
+            {suggestions.length===0?(
+                <div className="spinner-border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+            ):(
+                <Slider {...settings}>
+                {suggestions.map(current=>(
+                        <div className="out" key={current.id}>
+                            <div className="card">
+                                <img className="rounded-circle" alt={"users here"} src={`https://source.unsplash.com/random/${current.id}`} height={160} width={160} />
+                                
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+            )}
+        </div>
+    );
+}
+
+export default App;
